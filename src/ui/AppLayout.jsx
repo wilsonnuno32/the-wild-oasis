bf88c5572cdd02130f7625221e295dd7ef91e2e9
Nuno Gaspar <wilsonnuno@gmail.com>
2023-10-03 11:59:00 +0100

    handle form errors

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -13,6 +13,10 @@ const StyledAppLayout = styled.div`
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    -webkit-appearance: none;
+  }
 `;
 
 const Container = styled.div`
